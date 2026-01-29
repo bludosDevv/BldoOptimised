91b3f36bb9dc42ed872360ec49489d0123cc7530
BludosDevv <bludosofficial@gmail.com>
2026-01-29 18:57:47 +0530

    update BldoClientSetup.java

--- src/main/java/com/bludos/optimised/events/BldoClientSetup.java
+++ src/main/java/com/bludos/optimised/events/BldoClientSetup.java
@@ -2,15 +2,17 @@ package com.bludos.optimised.events;
 
 import com.bludos.optimised.runtime.BldoChunkBudget;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ClientTickEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BldoClientSetup {
 
     @SubscribeEvent
-    public static void onClientTick(ClientTickEvent e) {
-        BldoChunkBudget.schedulesThisTick = 0;
+    public static void onClientTick(TickEvent.ClientTickEvent e) {
+        if (e.phase == TickEvent.Phase.START) {
+            BldoChunkBudget.schedulesThisTick = 0;
+        }
     }
 }
